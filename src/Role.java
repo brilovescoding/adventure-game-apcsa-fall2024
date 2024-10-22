a1575a4f222d260f5956e5df5178cba191896797
Aubrey Schott <brilovescoding@gmail.com>
2024-10-22 11:27:45 -0400

    Added Rogue option

--- src/Role.java
+++ src/Role.java
@@ -1,16 +1,29 @@
 public class Role {
     private String name;
+    private int roleLevel;
     public Role(String _name) {
         if (_name.equals("Paladin")) {
             //set bonusHardiness to 2
             //set bonusWisdom to 0
         }
-        else if (_name.equals("Rogue")) {
+        else if (_name.equals("Rogue") || _name.equals("Assassin")) {
 
         }
         else {
             throw new java.lang.Error("Oops");
         }
+
+        if (10 > roleLevel && roleLevel > 5) {
+
+        }
+    }
+
+    public void setName(String _name) {
+        name = _name;
+    }
+
+    public String getName() {
+        return name;
     }
 }
 //nbjdskubgdbsuakbgdasukgas
